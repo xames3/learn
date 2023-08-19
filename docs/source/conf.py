3c0d2f1b56594bb88957c099599748c62b38fba0
Akshay "XA" Mestry <xa@mes3.dev>
2023-08-19 09:25:44 -0500

    chore: add support for jquery in Sphinx
    Signed-off-by: Akshay "XA" Mestry <xa@mes3.dev>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@ L.E.A.R.N Sphinx Configuration
 
 Author: Akshay Mestry <xa@mes3.dev>
 Created on: Wednesday, April 12 2023
-Last updated on: Thursday, July 27 2023
+Last updated on: Saturday, August 12 2023
 
 This file contains the configuration settings for building the L.E.A.R.N
 documentation using Sphinx, a popular Python documentation tool. Sphinx
@@ -246,6 +246,7 @@ extensions = [
     "sphinx_copybutton",
     "sphinx_design",
     "sphinx_favicon",
+    "sphinxcontrib.jquery",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
