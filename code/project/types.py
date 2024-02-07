d704a973342122ab8625b105e1c042516fe0e6be
Akshay "XA" Mestry <xa@mes3.dev>
2024-02-07 12:10:44 -0600

    chore: upstream supported types
    Signed-off-by: Akshay "XA" Mestry <xa@mes3.dev>

--- code/project/types.py
+++ code/project/types.py
@@ -4,7 +4,7 @@ Generic Project Types
 
 Author: Akshay Mestry <xa@mes3.dev>
 Created on: Friday, February 02 2024
-Last updated on: Friday, February 02 2024
+Last updated on: Tuesday, February 06 2024
 
 :copyright: (c) 2024 Akshay Mestry. All rights reserved.
 :license: MIT, see LICENSE for more details.
@@ -18,6 +18,7 @@ import types
 import typing as t
 
 import matplotlib.typing
+import numpy as np
 
 _T = t.TypeVar("_T", bound=t.Callable[..., t.Any])
 _VT = t.TypeVar("_VT")
@@ -33,3 +34,6 @@ _Stream = t.IO[str]
 _ExceptionInformation = (
     tuple[type, BaseException, types.TracebackType | None] | tuple[None, ...]
 )
+_SupportedFormats = t.Literal["png", "jpeg", "bmp"]
+_Image = np.ndarray
+_Dataset = list[tuple[int, _Path]]
